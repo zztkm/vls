bc6927b3bf655e043a88e42782d7392808444d2b
zztkm <takumi.basket1682@gmail.com>
2020-10-31 21:07:04 +0900

    Format vls.v

--- vls.v
+++ vls.v
@@ -3,25 +3,22 @@ module main
 import cli
 import os
 
-fn main() {
-	mut cmd := cli.Command {
-		name: 'vls',
-		description: 'ls commmad alternative written in V.',
-		version: '0.1.0',
-		execute: find_list_dir_contents
-	}
-
-	cmd.parse(os.args)
-}
-
 fn find_list_dir_contents(cmd cli.Command) {
 	cwd := os.getwd()
-
 	files := os.ls(cwd) or {
 		return
 	}
-
 	for file in files {
 		println(file)
 	}
+}
+
+fn main() {
+	mut cmd := cli.Command{
+		name: 'vls'
+		description: 'ls commmad alternative written in V.'
+		version: '0.1.0'
+		execute: find_list_dir_contents
+	}
+	cmd.parse(os.args)
 }
