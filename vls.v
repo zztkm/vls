62ccb437994284f8e4bd6b4aa5bbdd8e14275b8a
zztkm <zztkm@tsurutatakumi.info>
2024-11-28 20:58:17 +0900

    V 0.4.8 153b7f0 でビルドできるようにする

--- vls.v
+++ vls.v
@@ -3,11 +3,9 @@ module main
 import cli
 import os
 
-fn find_list_dir_contents(cmd cli.Command) {
+fn find_list_dir_contents(cmd cli.Command) ! {
 	cwd := os.getwd()
-	files := os.ls(cwd) or {
-		return
-	}
+	files := os.ls(cwd) or { return }
 	for file in files {
 		println(file)
 	}
@@ -15,10 +13,10 @@ fn find_list_dir_contents(cmd cli.Command) {
 
 fn main() {
 	mut cmd := cli.Command{
-		name: 'vls'
+		name:        'vls'
 		description: 'ls commmad alternative written in V.'
-		version: '0.1.0'
-		execute: find_list_dir_contents
+		version:     '0.1.0'
+		execute:     find_list_dir_contents
 	}
 	cmd.parse(os.args)
 }
